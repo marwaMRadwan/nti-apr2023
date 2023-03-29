78de0ba6c344f41447dcf448ff441366d55abaab
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-03-29 11:45:21 +0200

    s4

--- s4/src.js
+++ s4/src.js
@@ -1,17 +1,60 @@
-const apiUrl = "https://dummyjson.com/products"
+// const apiUrl = "https://dummyjson.com/products"
 // fetch(apiUrl)
 // .then( data => 
 //     data.json().then(
 //         res => console.log(res)
 // ) )
 // .catch( err => console.log( err ) )
+//posts, carts, products
+// const apiRes = async(apiUrl, cb) => {
+//     const data = await fetch(apiUrl)
+//     const res = await data.json()
+//     cb(res)
+// }
+
+// apiRes(apiUrl, res=> console.log(res))
+
+const btns = ["products", "carts", "posts"]
+const apiBaseURL ="https://dummyjson.com/"
+const btnWrap = document.querySelector("#btnWrap")
 
 const apiRes = async(apiUrl, cb) => {
-    const data = await fetch(apiUrl)
-    const res = await data.json()
-    cb(res)
+    try{
+        const data = await fetch(apiUrl)
+        const res = await data.json()
+        cb(res , null)
+    }
+    catch(e){
+        cb(null, e.message)
+    }
 }
 
+btns.forEach(btn=>{
+    mybtn = document.createElement("button")
+    btnWrap.appendChild(mybtn)
+    mybtn.textContent = btn
+    mybtn.classList = "mx-3 btn btn-primary"
+    mybtn.addEventListener("click", ()=>{
+        apiRes(`${apiBaseURL}${btn}`, (res, err)=>{
+            if(err) console.log(err)
+            else console.log(res);
+        })
+    })
+})
 
-apiRes(apiUrl, res=> console.log(res))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
