b5d08fa419f3d5db63312318bdf9039f735b80a1
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-04-02 15:13:54 +0200

    s6

--- s6/expressApp/controller/src.js
+++ s6/expressApp/controller/src.js
@@ -3,7 +3,10 @@ const app = express()
 const path = require("path")
 //engine => hbs 
 //path
+const staticDir = path.join(__dirname , "../public")
 const viewsDir = path.join(__dirname , "../resources/views")
+
+app.use(express.static(staticDir))
 
 app.set("view engine", "hbs")
 app.set("views", viewsDir)
